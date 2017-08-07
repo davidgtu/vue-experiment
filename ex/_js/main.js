ff79e55ea0418b5ddddec7c6f7eca769b46a82f1
davidgtu <david.giahuy.tu@gmail.com>
2017-08-07 10:24:27 -0400

    add skeleton

---
+++ ex/_js/main.js
@@ -0,0 +1,32 @@
+//requires setup-6.0.js or higher
+var TM = {
+
+	loadLibs: function(){
+		console.log('loadLibs called')
+
+		//most of these could be in cache.mf
+		return Promise.all([
+			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js')
+			, TS.load('//rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-latest.js')
+		])
+		.then(TM.libsLoaded)
+	}
+
+	, libsLoaded: function(){
+		
+		TS.signalAppReady()
+
+		TT.ScontentID = '#content-wrapper'
+		TT.handle(function(evt) {
+			if(TT.PRE == evt.typ)  {
+				//$('#content-wrapper').fadeTo(100,.2)
+			}
+			if(TT.PAGE == evt.typ)  {
+				$(TT.ScontentID).html(evt.$new)
+			}
+		})
+	}
+
+} //class
+
+TM.loadLibs()
